c27cb3c12f1f0e9dc574e6250d40d0d289a2f075
gagneja1512 <aadityaarora1215@gmail.com>
2022-06-11 23:27:12 +0530

    Adding input component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,5 @@
 import classes from './MealItem.module.css'
-
+import MealItemForm from './MealItemForm';
 const MealItem = (props) => {
 
     const price = `$${props.price.toFixed(2)}` ;
@@ -17,7 +17,7 @@ const MealItem = (props) => {
             </div>
         </div>
         <div>
-
+            <MealItemForm id = {props.id}/>
         </div>
     </li>
 };
