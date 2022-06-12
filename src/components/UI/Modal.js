176108a17b32cd5b68e9b03bdb1c51c08a93ce44
gagneja1512 <aadityaarora1215@gmail.com>
2022-06-12 15:43:09 +0530

    Adding states

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,7 +4,7 @@ import reactDom from 'react-dom';
 
 const Backdrop = (props) => {
     return (
-        <div className={classes.backdrop}></div>
+        <div className={classes.backdrop} onClick={props.onClose}></div>
     )
 };
 
@@ -21,7 +21,7 @@ const portalElement = document.getElementById('overlays') ;
 const Modal = (props) => {
     return (
         <Fragment>
-            {reactDom.createPortal(<Backdrop/> , portalElement)}
+            {reactDom.createPortal(<Backdrop onClose={props.onClose} /> , portalElement)}
             {reactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay> , portalElement)}
         </Fragment>
     )
