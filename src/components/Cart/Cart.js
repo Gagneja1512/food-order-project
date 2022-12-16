466b37fdda10e1a681c7db99fa8e3315d35aabe0
gagneja1512 <aadityaarora1215@gmail.com>
2022-12-16 23:25:38 +0530

    Enhancing Cart features

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -46,6 +46,8 @@ const Cart = (props) => {
         })
         setIsSubmitting(false)
         setDidSubmit(true)
+
+        cartCtx.clearCart()
     }
 
     const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map(item => <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)} />)}</ul>;
